23eb42ae6510c63f51352e27ab5dbb1f1f928b2c
PIYUSH SINGH <37767023+DataVoyagerX30@users.noreply.github.com>
2025-08-20 02:28:44 +0100

    Add files via upload

---
+++ 02_feature_engineering.sql
@@ -0,0 +1,19 @@
+
+-- Add time_of_day column and update values
+ALTER TABLE sales ADD time_of_day VARCHAR(20);
+UPDATE sales 
+SET time_of_day = (
+    CASE 
+        WHEN time BETWEEN '00:00:00' AND '12:00:00' THEN 'MORNING'
+        WHEN time BETWEEN '12:01:00' AND '16:00:00' THEN 'AFTERNOON'
+        ELSE 'EVENING'
+    END
+);
+
+-- Add day_name column and update values
+ALTER TABLE sales ADD COLUMN day_name VARCHAR(10);
+UPDATE sales SET day_name = DAYNAME(date);
+
+-- Add month_name column and update values
+ALTER TABLE sales ADD COLUMN month_name VARCHAR(10);
+UPDATE sales SET month_name = MONTHNAME(date);
